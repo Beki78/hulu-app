65c4aabec0332ee4c16775d07bacee883f225097
Bereket Getachew <146061009+Beki78@users.noreply.github.com>
2024-05-04 21:01:14 +0300

    first commit

---
+++ hulu-app/src/components/Footer.tsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+  return ( 
+    <div className="w-full bg-black text-gray-400 text-center py-2">
+      <small>© 2024 Bereket Getachew. All rights reserved.</small>
+    </div>
+  );
+}
+
+export default Footer
---
+++ hulu-app/src/screen/Home.tsx
@@ -0,0 +1,17 @@
+import Header from '../components/Header'
+import Hero from '../components/Hero'
+import GenereMovieList from '../components/GenereMovieList'
+import Footer from '../components/Footer'
+
+const Home = () => {
+  return (
+    <div>
+      <Header/>
+      <Hero/>
+      <GenereMovieList/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default Home
